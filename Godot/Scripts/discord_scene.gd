bf6a1c813a75bd3c6b3693f603604ce04bc28db8
BrawlTest <158631597+Frakif91@users.noreply.github.com>
2025-05-27 09:51:38 +0200

    eee

--- Godot/Scripts/discord_scene.gd
+++ Godot/Scripts/discord_scene.gd
@@ -1,5 +1,7 @@
 class_name DiscordMenu extends Control
 
+signal popup_request_status_changed(status : POPUP_STATUS)
+
 @onready var server_list : ItemList = %ServerList
 @onready var server_info : RichTextLabel = %ServerInfo
 @onready var channel_name : Label = %ChannelName
@@ -9,3 +11,14 @@ class_name DiscordMenu extends Control
 @onready var guild_list : ItemList = %GuildList
 @onready var channel_chat : VBoxContainer = %ChannelChat
 @onready var chat_input : LineEdit = %ChatInput
+
+enum POPUP_STATUS {POPUP_ACCEPT, POPUP_DECLINED, POPUP_CLOSED}
+
+func _ready() -> void:
+    $"PopupPanel".close_requested.connect(_on_popup_close)
+    $"PopupPanel/VBoxContainer/HBoxContainer/AddButtonServer".pressed.connect(func(): popup_request_status_changed.emit(POPUP_STATUS.POPUP_ACCEPT); $"PopupPanel".hide())
+    $"PopupPanel/VBoxContainer/HBoxContainer/Cancel".pressed.connect(func(): popup_request_status_changed.emit(POPUP_STATUS.POPUP_DECLINED); $"PopupPanel".hide())
+
+func _on_popup_close():
+    $"PopupPanel".hide()
+    emit_signal("popup_request_status_changed", POPUP_STATUS.POPUP_CLOSED)
